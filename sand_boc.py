edfe1820393fc948a76eeb580d89d4a9daa8e1c4
datasandwich <72459044+datasandwich@users.noreply.github.com>
2020-12-13 06:50:18 +0000

    Update sand_boc.py

--- sand_boc.py
+++ sand_boc.py
@@ -8,10 +8,11 @@ This is a temporary script file.
 #user_input = input("Please enter a name: ")
 
 class RentalShop:
-    def __init__(self):
-        self.stock = {"Hb":{'quantity':4,'price':{'week':30,'week+':25}},
-                      'Sed':{'quantity':3,'price':{'week':50,'week+':40}},
-                      'SUV':{'quantity':3,'price':{'week':100,'week+':90}}} 
+    stock = {"Hb":{'quantity':4,'price':{'week':30,'week+':25}},
+         'Sed':{'quantity':3,'price':{'week':50,'week+':40}},
+         'SUV':{'quantity':3,'price':{'week':100,'week+':90}}} 
+    #def __init__(self):
+        
 
     def display_inventory_and_prices(self):
         return self.stock
@@ -44,31 +45,33 @@ class Customer:
         print(self.shop.display_inventory_and_prices())
     
     def rent_car(self):
-        car_type=input('car type')
-        days=input('days')
-        self.shop.process_rent_request(car_type,days)
+        self.car_type=input('car type')
+        self.days=input('days')
+        self.shop.process_rent_request(self.car_type,self.days)
         
     def return_car(self):
-        car_type=input('car type')
-        days=input('days')
-        self.shop.process_return_request(car_type,days)
+        self.shop.process_return_request(self.car_type,self.days)
 
         
         
 """"Here I'd like to use the input to create some sort of class instance name, and also declare the instance's name"""
+'''
 
-customer_db={}
 def main_menu():
     
     user_input=input('enter name')
-    customer =Customer(name=user_input)
+
     customer_db[user_input]=customer
 
 '''
+my_object=Customer('dave')
+my_other_object=Customer('john')
 print(my_object)
 print(my_object.inquire())
 print(my_object.rent_car())
+print(my_other_object.rent_car())
+print(my_other_object.return_car())
 print(my_object.return_car())
-
+#print(my_object.return_car())
 
     
