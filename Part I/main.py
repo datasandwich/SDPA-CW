13e06d86c68756a2364c39b929d763eb1f722585
datasandwich <72459044+datasandwich@users.noreply.github.com>
2020-12-14 16:48:12 +0000

    Update main.py

--- Part I/main.py
+++ Part I/main.py
@@ -3,25 +3,15 @@
 """
 Created on Fri Dec 11 15:03:19 2020
 
-@author: datasandwich
+@author: datasandwich (Jarek Ettl, of20499)
+@description:
+    Calls the login function from the Menu class
+    
 """
-
 
 from Menu import Menu
 
 Menu.login(Menu.customers)
 
-'''
-Errors
-
-check customer name doesn't contain numbers
-
-
-MAKE SURE
-
-Every function written is commented, in your own words, using a doc-string format that describes its behavior, parameters, returns, and highlights any special cases.
-There is a comment at the top of each code file you write with your name, section, and a brief description of what that program does.
-
-'''
 
 
