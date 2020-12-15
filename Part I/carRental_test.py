53cac66d95be17e5f3c2137a189d3e7d2e6fbd05
datasandwich <72459044+datasandwich@users.noreply.github.com>
2020-12-15 10:14:39 +0000

    Minor changes

--- Part I/carRental_test.py
+++ Part I/carRental_test.py
@@ -3,6 +3,27 @@
 """
 Created on Fri Dec 11 15:04:31 2020
 
-@author: datasandwich
+@author: datasandwich (Jarek Ettl, of20499)
+@description: Unit test script for car rental program
 """
 
+import unittest
+from Rental_shop import RentalShop
+from Menu import Menu
+
+class RentalShopTestCase(unittest.TestCase):
+    """Tests for Rental_shop.py"""
+    
+    
+class MenuTestCase(unittest.TestCase):
+    """Tests for Menu.py"""
+    
+    
+    
+if __name__ == '__main__':
+    unittest.main()
+    
+    
+'''
+Use just one class?
+'''
